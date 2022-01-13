dc179bba7a09437f586f124d5e5577a55f1a1439
Seungtae Lee <90381057+SingToLive@users.noreply.github.com>
2022-01-13 23:54:30 +0900

    update to 3.5
    lecture update to 3.5

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
-const age = parseInt(prompt("How old are you"));
+const title = document.getElementById("title");
 
-console.log(isNaN(age));
-
-if(condition){
-    ///// condition === ture
-} else {
-    /// condition === false
+function handleTitleClick() {
+    title.style.color = "blue"
 }
+
+function handleMouseEnter() {
+    title.innerText = "Here is mouse";
+}
+
+function handleMouseLeave() {
+    title.innerText = "Mouse is gone"
+}
+
+
+// title.addEventListener("click", handleTitleClick);
+title.onclick = handleTitleClick;
+// title.addEventListener("mouseenter", handleMouseEnter);
+title.onmouseenter = handleMouseEnter;
+title.addEventListener("mouseleave", handleMouseLeave)
+
+function handleWindowResize() {
+    document.body.style.backgroundColor = "tomato"
+}
+
+window.addEventListener("resize", handleWindowResize)
